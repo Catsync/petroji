2ec406ccb1c869b21ab372e865ff9b27f45e6e46
Rick Anderson <rick.rickanderson@gmail.com>
2015-01-24 18:34:27 -0500

    Auth changes for angular

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,8 @@
 angular.module('petroji',[
-	'ui.router'
-	// 'petroji.auth'
+	'ui.router',
+  'petroji.auth'
 ])
 .config(['$stateProvider', '$urlRouterProvider',function($stateProvider,$urlRouterProvider) {
 	"use strict";
-
 	$urlRouterProvider.otherwise('/auth');
-	$stateProvider
-		.state('auth', {
-			url: '/auth',
-			templateUrl: '/partials/login.tpl.html'
-		});
-
-}])
-;
+}]);
