d4fc79cc3c6411a83e01d40a6787b00241c28fd6
Cat Sync <catsync@zdh.com>
2015-01-25 01:47:54 -0500

    Cleaning up

--- src/js/village/villageService.js
+++ src/js/village/villageService.js
@@ -14,29 +14,17 @@ angular.module('petroji')
 		if(!id) { return null; }
 		if(village !== null && village.$id == id) { return village; }
 		village = Village(id);
-		console.log(village);
 		return village;
 	};
 
 	villageService.newVillage = function() {
 		village = Village();
-		console.log(village);
 		return village;
-		// var v = {};
-		// var props = villageService.VALID_PROPERTIES;
-		// for(var i=0; i< props.length; i++) {
-		// 	v[props[i]] = villageDefault[props[i]];
-		// }
-
-		// return activeVillagesSync.$push(v).then(function(newRef) {
-		// 	village = $firebase(newRef).$asObject();
-		// });
 	};
 
 	villageService.addUsertoVillage = function(userId) {
 		console.log('addUserToVillage called: '+userId);
 		if(village === null) { return false; }
-
 
 	};
 
