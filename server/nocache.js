1573c7a41138a0744825be978997885ffa032db0
Cat Sync <catsync@zdh.com>
2015-01-24 20:59:40 -0500

    Basic node development server added

---
+++ server/nocache.js
@@ -0,0 +1,9 @@
+
+// Middleware to turn off http caching
+
+module.exports = function(req,res,next) {
+	res.setHeader('Cache-Control','no-cache, no-store, must-revalidate');
+	res.setHeader('Pragma','no-cache');
+	res.setHeader('Expires','0');
+	next();
+};
