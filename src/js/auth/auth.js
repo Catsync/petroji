47db20e4d19a881748fa6264f1fff1f384ee81cf
ricklon <rick.rickanderson@gmail.com>
2015-01-24 20:59:37 -0500

    updated login form

--- src/js/auth/auth.js
+++ src/js/auth/auth.js
@@ -1,4 +1,5 @@
 angular.module('petroji.auth',[
+  "firebase"
 ])
 .config(['$stateProvider', '$urlRouterProvider',function($stateProvider,$urlRouterProvider) {
   "use strict";
@@ -11,5 +12,19 @@ angular.module('petroji.auth',[
 }])
 .controller("authController", [function() {
   console.log("authController");
-  this.user={"name": "foo"};
+  //this.user={"name": "foo"};
 }]);
+/*
+
+var ref = new Firebase("https://<your-firebase>.firebaseio.com");
+ref.createUser({
+email    : "bobtony@firebase.com",
+password : "correcthorsebatterystaple"
+}, function(error) {
+if (error === null) {
+console.log("User created successfully");
+} else {
+console.log("Error creating user:", error);
+}
+});
+*/
